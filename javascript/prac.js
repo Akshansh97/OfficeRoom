e6b42e454e046193234a2e7bd1d6fe5264b41075
Akshansh <akshanshdubey.dev@gmail.com>
2025-08-30 23:54:02 +0530

    changes in prac.js

--- javascript/prac.js
+++ javascript/prac.js
@@ -1,41 +1,17 @@
-// function myFunc({a, b}) { console.log(a, b); } myFunc({a: 1, b: 2});
-//const object1 = { property1: 20 }; console.log(Object.is(object1));
-
-//var materials = [ 'Table', 'Chair', 'Boxes', 'Press' ]; console.log(materials.map(material => material.length));
-// const prototype1 = {}; const object1 = Object.create(prototype1); console.log(Object.getPrototypeOf(object1) === prototype1);
-// var add=new Function("num1","num2","return num1+num2"); console.log(add(2,5));
-// var string2Num=parseInt("123xyz");
-// console.log(string2Num)
-
-// const object1 = {};
-// a = Symbol("a");
-// b = Symbol.for("b");
-// object1[a] = "harry";
-// object1[b] = "derry";
-// const objectSymbols = Object.getOwnPropertySymbols(object1);
-// console.log(objectSymbols.length);
-
-//  function sayHi() { return (() => 0)(); } console.log(typeof sayHi());
-
-// const obj1 = { property1: 2 }; Object.seal(obj1); obj1.property1 =4; console.log(obj1.property1); delete obj1.property1;
-
-// const pt1 = {}; const ob1 = Object.create(pt1); console.log( Object.getPrototypeOf(ob1) === pt1 );
-
-// var count = [1,,3]
-// console.log(count)
-
-function winorlose(s) {
-  // Your code goes here
-  let count1 = 0,
-    count2 = 0;
-  for (let i = 0; i < s.length; i++) {
-    if (s[i] === 1) {
-      count1++;
-    } else count2++;
+var findRepeatedDnaSequences = function (s) {
+  const seen = new Set();
+  const repeated = new Set();
+  for (let i = 0; i <= s.length - 10; i++) {
+    const seq = s.substring(i, i + 10);
+    if (seen.has(seq)) {
+      repeated.add(seq);
+    } else {
+      seen.add(seq);
+    }
   }
-  if (count1 > count2) {
-    return "WIN";
-  } else return "LOSE";
-}
+  return Array.from(repeated);
+};
 
-console.log(winorlose("0101111111111"));
+// Example usage:
+console.log(findRepeatedDnaSequences("AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT"));
+//
